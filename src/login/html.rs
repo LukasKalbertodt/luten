e00c56a7be3730558bdf8ec4ac9b0a8c1f69eb41
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2017-08-24 17:20:43 +0200

    Properly Design login-form
    This commit also includes "font awesome" for icons

--- src/login/html.rs
+++ src/login/html.rs
@@ -2,17 +2,50 @@ use maud::{html, Markup};
 
 pub fn content() -> Markup {
     html! {
-        div class="login-box" {
-            h2 "Einloggen"
-            hr;
-            form method="post" action="/login" class="basgit-form" {
-                input type="text" name="id" placeholder="Nutzerkennung";
-                input type="password" name="secret" placeholder="Password";
-                input type="submit" value="Login";
-            }
+        div class="o-container o-container--xsmall@small u-letter-box--medium" {
+            div class="c-card" {
+                div class="c-card__item c-card__item--brand" "Einloggen"
+                form method="post" action="/login" class="u-window-box--small" {
+                    // Field: "username"
+                    div class="u-letter-box--small login-field-container" {
+                        div class="o-field o-field--icon-left" {
+                            i class="fa fa-fw fa-user c-icon" {}
+                            input
+                                type="text"
+                                class="c-field"
+                                name="id"
+                                placeholder="Nutzerkennung";
+                            div class="c-hint u-small" {
+                                "Gib hier deine RZ-Kennung ein (z.B. "
+                                i "xmuster"
+                                ")"
+                            }
+                        }
+                    }
 
-            hr;
-            p "Uhm..."
+                    // Field: "password"
+                    div class="u-letter-box--small login-field-container" {
+                        div class="o-field o-field--icon-left" {
+                            i class="fa fa-fw fa-key c-icon" {}
+                            input
+                                type="password"
+                                class="c-field"
+                                name="secret"
+                                placeholder="Password";
+                            div class="c-hint u-small" "Dein globales Uni-Password"
+                        }
+                    }
+
+                    // Button: "Login"
+                    div class="u-letter-box--small" {
+                        input
+                            type="submit"
+                            value="Login"
+                            class="c-button c-button--success"
+                            style="width: 100%";
+                    }
+                }
+            }
         }
     }
 }
--- src/template/mod.rs
+++ src/template/mod.rs
@@ -96,6 +96,8 @@ impl Page {
             // ===============================================================
             head {
                 link rel="stylesheet" href="/static/blaze@3.3.0.css";
+                link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/\
+                    font-awesome/4.7.0/css/font-awesome.min.css"
                 link rel="stylesheet" href="/static/main.css";
                 title {
                     (self.title)
