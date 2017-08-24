56b74744c0f6e58ca9129f3bd4f401b74a24128d
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2017-08-24 23:54:27 +0200

    Add notice-box to login page

--- src/login/html.rs
+++ src/login/html.rs
@@ -2,50 +2,81 @@ use maud::{html, Markup};
 
 pub fn content() -> Markup {
     html! {
-        div class="o-container o-container--xsmall@small u-letter-box--medium" {
-            div class="c-card" {
-                div class="c-card__item c-card__item--brand" "Einloggen"
-                form method="post" action="/login" class="u-window-box--small" {
-                    // Field: "username"
-                    div class="u-letter-box--small login-field-container" {
-                        div class="o-field o-field--icon-left" {
-                            i class="fa fa-fw fa-user c-icon" {}
-                            input
-                                type="text"
-                                class="c-field"
-                                name="id"
-                                placeholder="Nutzerkennung";
-                            div class="c-hint u-small" {
-                                "Gib hier deine RZ-Kennung ein (z.B. "
-                                i "xmuster"
-                                ")"
+        div class="o-grid o-grid--small-full u-letter-box--small" {
+            // Empty grid cell to have a blank buffer on the side
+            div class="o-grid__cell o-grid__cell--no-gutter" {}
+
+            // Actual login-box
+            div
+                class="o-grid__cell o-grid__cell--width-fixed u-letter-box--small"
+                style="width: 25em"
+            {
+                div class="c-card" {
+                    div class="c-card__item c-card__item--brand c-card__item--divider" "Einloggen"
+                    div class="c-card__item" {
+                        form method="post" action="/login" class="u-pillar-box--small" {
+                            // Field: "username"
+                            div class="u-letter-box--small login-field-container u-large" {
+                                div class="o-field o-field--icon-left" {
+                                    i class="fa fa-fw fa-user c-icon" {}
+                                    input
+                                        type="text"
+                                        class="c-field"
+                                        name="id"
+                                        placeholder="Nutzerkennung";
+                                    div class="c-hint" {
+                                        "Deine RZ-Kennung (z.B. "
+                                        i "xmuster"
+                                        ")"
+                                    }
+                                }
+                            }
+
+                            // Field: "password"
+                            div class="u-letter-box--small login-field-container u-large" {
+                                div class="o-field o-field--icon-left" {
+                                    i class="fa fa-fw fa-key c-icon" {}
+                                    input
+                                        type="password"
+                                        class="c-field"
+                                        name="secret"
+                                        placeholder="Password";
+                                    div class="c-hint" "Dein globales Uni-Password"
+                                }
+                            }
+
+                            // Button: "Login"
+                            div class="u-letter-box--small" {
+                                input
+                                    type="submit"
+                                    value="Login"
+                                    class="c-button c-button--success"
+                                    style="width: 100%";
                             }
                         }
                     }
+                }
+            }
 
-                    // Field: "password"
-                    div class="u-letter-box--small login-field-container" {
-                        div class="o-field o-field--icon-left" {
-                            i class="fa fa-fw fa-key c-icon" {}
-                            input
-                                type="password"
-                                class="c-field"
-                                name="secret"
-                                placeholder="Password";
-                            div class="c-hint u-small" "Dein globales Uni-Password"
+            // Notice box
+            div
+                class="o-grid__cell o-grid__cell--width-fixed u-letter-box--small"
+                style="width: 30em"
+            {
+                div class="c-card" {
+                    div class="c-card__item c-card__item--divider" "Hinweise"
+                    div class="c-card__item" {
+                        ul class="login-notice-list" {
+                            li "Nutze zum Einloggen deine normalen Uni-Login-Daten"
+                            li "Die Authentifizierung erfolgt über das Uni-LDAP"
+                            li "Dein Password wird zu keiner Zeit gespeichert"
                         }
-                    }
-
-                    // Button: "Login"
-                    div class="u-letter-box--small" {
-                        input
-                            type="submit"
-                            value="Login"
-                            class="c-button c-button--success"
-                            style="width: 100%";
                     }
                 }
             }
+
+            // Empty grid cell to have a blank buffer on the side
+            div class="o-grid__cell o-grid__cell--no-gutter" {}
         }
     }
 }
