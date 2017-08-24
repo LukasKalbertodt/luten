1114ded90bc55e319b10f98dfb0f31f12790d55a
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2017-08-24 23:59:10 +0200

    Add "viewport" meta tag

--- src/template/mod.rs
+++ src/template/mod.rs
@@ -97,8 +97,9 @@ impl Page {
             head {
                 link rel="stylesheet" href="/static/blaze@3.3.0.css";
                 link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/\
-                    font-awesome/4.7.0/css/font-awesome.min.css"
+                    font-awesome/4.7.0/css/font-awesome.min.css";
                 link rel="stylesheet" href="/static/main.css";
+                meta name="viewport" content="width=device-width, initial-scale=1.0";
                 title {
                     (self.title)
                     ({
