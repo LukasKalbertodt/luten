e5f16254c24fc00c71ee0f2132cc6df38bcbfdbf
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2017-09-03 21:43:31 +0200

    Add `add_dummies` binary

---
+++ src/bin/add_dummies.rs
@@ -0,0 +1,64 @@
+//! Helper utility to add three dummy users to the database.
+//!
+//! Note that this doesn't add password logins for these users. Add those
+//! manually via `manage_db create password`.
+
+extern crate luten;
+extern crate term_painter;
+
+
+use std::cmp::min;
+
+use term_painter::{Color, ToStyle};
+
+use luten::db::Db;
+use luten::errors::*;
+use luten::user::{Role, User};
+
+
+fn main() {
+    // Establish DB connection
+    let db = Db::open_connection();
+
+    // Insert three users
+    let do_it = || -> Result<()> {
+        let student = User::create(
+            "dummy_student".into(),
+            Some("Willi Wacker".into()),
+            Role::Student,
+            &db,
+        )?;
+        let tutor = User::create(
+            "dummy_tutor".into(),
+            Some("Susi Sorglos".into()),
+            Role::Tutor,
+            &db,
+        )?;
+        let admin = User::create(
+            "dummy_admin".into(),
+            Some("Admin Peter".into()),
+            Role::Admin,
+            &db,
+        )?;
+
+        println!("Inserted:");
+        println!("{:?}", student);
+        println!("{:?}", tutor);
+        println!("{:?}", admin);
+
+        Ok(())
+    };
+
+    // Pretty print error chain
+    if let Err(error_chain) = do_it() {
+        println!("Something went wrong ☹ ! Here is the backtrace:");
+        for (i, e) in error_chain.iter().enumerate() {
+            println!(
+                "{: >2$} {}",
+                Color::Yellow.paint(if i == 0 { "→" } else { "⤷" }),
+                Color::Red.paint(e),
+                2 * min(i, 7) + 1,
+            );
+        }
+    }
+}
