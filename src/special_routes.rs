8df037772988651e0456229875d54cf344cfa483
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2017-09-03 23:34:52 +0200

    Improve handling of Unauthorized/Forbidden
    - Instead of 401 Unauthorized, 403 Forbidden is used. The former
      should only be used for HTTP-basic auth.
    - The 403 catcher checks if a login is present at all. If not, the
      user is forwarded to `/login` (as before). If a login is
      present, an error will be shown.

--- src/special_routes.rs
+++ src/special_routes.rs
@@ -1,10 +1,15 @@
 use std::path::{Path, PathBuf};
 
+use maud::{html, Markup};
 use rocket::response::{Flash, NamedFile, Redirect};
-use rocket::Request;
+use rocket::{Outcome, Request};
 use rocket::http::Cookie;
 
 use config;
+use dict::{self, Locale};
+use errors::*;
+use template::{Flash as OurFlash, Page};
+use user::AuthUser;
 
 /// Route to serve static file requests from the `static/` directory.
 ///
@@ -14,18 +19,41 @@ pub fn static_files(path: PathBuf) -> Option<NamedFile> {
     NamedFile::open(Path::new("static/").join(path)).ok()
 }
 
-/// Redirect to `/login` if a route is requested that cannot be access when not
-/// logged in (most routes).
-#[error(401)]
-fn unauthorized(req: &Request) -> Flash<Redirect> {
-    let uri = req.uri().as_str().to_owned();
-    let cookie = Cookie::build(config::INITIAL_REQ_COOKIE_NAME, uri)
-        .path("/")
-        .finish();
-    req.cookies().add(cookie);
 
-    Flash::error(
-        Redirect::to("/login"),
-        "Du musst eingeloggt sein, um die angefragte Seite zu sehen."
-    )
+/// Catcher for 403 Forbidden.
+///
+/// Redirect to `/login` if there is no login present. Show a big red error
+/// otherwise.
+#[error(403)]
+fn unauthorized(req: &Request) -> StdResult<Flash<Redirect>, Markup> {
+    if let Outcome::Success(auth_user) = req.guard::<AuthUser>() {
+        // In this case a login IS present, but it lacks the permissions to do
+        // something. In this case it doesn't make sense to forward to the
+        // login page. We will show an error instead.
+        let locale = req.guard::<Locale>().unwrap();
+
+        let page = Page::empty()
+            .with_auth_user(&auth_user)
+            .add_flashes(vec![
+                OurFlash::error(html! {
+                    (dict::new(locale).forbidden_flash())
+                }),
+            ])
+            .render();
+
+        Err(page)
+    } else {
+        // In this case, there is no login present. We will forward to the
+        // login page.
+        let uri = req.uri().as_str().to_owned();
+        let cookie = Cookie::build(config::INITIAL_REQ_COOKIE_NAME, uri)
+            .path("/")
+            .finish();
+        req.cookies().add(cookie);
+
+        Ok(Flash::error(
+            Redirect::to("/login"),
+            "Du musst eingeloggt sein, um die angefragte Seite zu sehen."
+        ))
+    }
 }
--- src/user/mod.rs
+++ src/user/mod.rs
@@ -124,7 +124,7 @@ impl<'a, 'r> FromRequest<'a, 'r> for AuthUser {
         match Session::from_cookies(req.cookies(), &db) {
             Err(e) => Outcome::Failure((Status::InternalServerError, Some(e))),
             Ok(Some(auth_user)) => Outcome::Success(auth_user),
-            Ok(None) => Outcome::Failure((Status::Unauthorized, None)),
+            Ok(None) => Outcome::Failure((Status::Forbidden, None)),
         }
     }
 }
