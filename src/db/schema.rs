9123bb1a1fc62ac72f1aea4baf3fe28636b715de
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2017-09-03 19:28:31 +0200

    Switch to `table!()` based schema definition
    We cannot use `infer_schema!()` with enum type right now, so we
    need to switch for that reason anyway. But apart from that, the
    `infer_schema!()` macro was a bit *too* magical for my taste anyway.
    Defining the schema via `table!()` manually is a bit more work and
    leads to duplication (definition in DB and in code), but I think
    it's a good choice.

--- src/db/schema.rs
+++ src/db/schema.rs
@@ -1,1 +1,24 @@
-infer_schema!("dotenv:DATABASE_URL");
+table! {
+    passwords (user_id) {
+        user_id -> Int8,
+        hash -> Bpchar,
+    }
+}
+joinable!(passwords -> users(user_id));
+
+table! {
+    sessions (id) {
+        id -> Bytea,
+        user_id -> Int8,
+        birth -> Timestamptz,
+    }
+}
+joinable!(sessions -> users(user_id));
+
+table! {
+    users (id) {
+        id -> Int8,
+        username -> Text,
+        name -> Nullable<Text>,
+    }
+}
