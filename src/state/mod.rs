cb0ca63deec56847a4e3c3920aa56f7a130cf138
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2017-09-05 22:57:12 +0200

    Add documentation for `state`

--- src/state/mod.rs
+++ src/state/mod.rs
@@ -1,3 +1,14 @@
+//! The state of the application.
+//!
+//! The website can be in different states. The current state influences which
+//! routes are available. The two main states are:
+//!
+//! - `Preparation`: this is where tutors and students specify their
+//!   preferences regarding time slots, ... This state is active only briefly
+//!   at the beginning of the semester.
+//! - `Running`: when all time slots and partners/tutors are assigned. This is
+//!   active during the semester.
+
 use chrono::DateTime;
 use chrono::offset::Utc;
 use rocket::{Outcome, State};
@@ -7,15 +18,19 @@ use rocket::request::{self, FromRequest, Request};
 use errors::*;
 use db::Db;
 
-
+/// All possible states of the app
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum AppState {
     Preparation,
     Running,
+
+    /// This is a helper state for administrators. It disable all meaningful
+    /// routes and can be used to ... calculate a schedule or stuff like that.
     Frozen,
 }
 
 
+/// Represents the current application state, as stored in the database.
 #[derive(Debug, Clone, Eq, PartialEq, Queryable)]
 pub struct CurrentAppState {
     id: bool,
@@ -25,6 +40,7 @@ pub struct CurrentAppState {
 }
 
 impl CurrentAppState {
+    /// Loads the current app state from the database.
     pub fn load(db: &Db) -> Result<Self> {
         use db::schema::current_app_state;
         use diesel::prelude::*;
@@ -42,7 +58,7 @@ impl CurrentAppState {
 
 macro_rules! state_req_guard {
     ($name:ident, $variant:ident) => {
-        /// A request guard to ensure the current app state is "$variant".
+        /// A request guard to ensure the app is in a specific state.
         pub struct $name(pub CurrentAppState);
 
         impl<'a, 'r> FromRequest<'a, 'r> for $name {
