951ccf0f0077a6e55e086441f450a88514ae1e0a
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2017-09-11 11:52:16 +0200

    Add TS compiling to build script and include resulting JS file in webpage

--- build.rs
+++ build.rs
@@ -6,27 +6,81 @@ use std::io::{self, ErrorKind, Write};
 /// The folder containing `main.scss`.
 const SCSS_DIR: &str = "scss/";
 
+/// The folder containing the TypeScript library.
+const TS_DIR: &str = "ts/";
+
 /// The output folder in which `main.css` will be written.
 const CSS_OUT_DIR: &str = "static/";
 
 
 fn main() {
     let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
-    compile_sass(&manifest_dir);
-}
-
-/// Compiles `.scss` files with `sass` which is assumed to be installed.
-fn compile_sass(manifest_dir: &Path) {
-    // In and out paths
-    let scss_dir = manifest_dir.join(SCSS_DIR);
-    let out_dir = manifest_dir.join(CSS_OUT_DIR);
 
     // If the application is being compiled in release mode, we want to minify
-    // the resulting CSS.
+    // the resulting CSS and JS.
     //
     // We might want to check for Rocket environments instead of the cargo
     // build profile later (TODO, see Rocket#228).
     let is_debug = env::var("PROFILE").unwrap() == "debug";
+
+    compile_sass(&manifest_dir, is_debug);
+    compile_ts(&manifest_dir, is_debug);
+}
+
+fn compile_ts(manifest_dir: &Path, is_debug: bool) {
+    // In and out paths
+    let ts_dir = manifest_dir.join(TS_DIR);
+
+    let flags = if is_debug {
+        &[] as &[_]
+    } else {
+        &["-p"]
+    };
+
+    // Execute the compiler
+    let res = Command::new(ts_dir.join("node_modules/.bin/webpack"))
+        .current_dir(ts_dir)
+        .args(flags)
+        .output();
+
+    // Check if anything went wrong
+    match res {
+        Err(e) => {
+            eprintln!("An IO error occured while running the scss-compiler:");
+            eprintln!(" >> {}", e);
+
+            if e.kind() == ErrorKind::NotFound {
+                eprintln!(
+                    "!! Make sure you have executed `npm install` in your `ts` folder. \
+                     Afterwards there should be a file `ts/node_modules/.bin/webpack`!");
+            }
+            eprintln!("");
+
+            panic!("error compiling ts files");
+        }
+        Ok(output) => {
+            if !output.status.success() {
+                eprintln!("`webpack` exited unsuccessful!");
+
+                eprintln!("--- stdout ---");
+                io::stderr().write_all(&output.stdout)
+                    .expect("IO error printing to stderr");
+                eprintln!("--- stderr ---");
+                io::stderr().write_all(&output.stderr)
+                    .expect("IO error printing to stderr");
+
+                panic!("error compiling ts files");
+            }
+        }
+    }
+}
+
+/// Compiles `.scss` files with `sass` which is assumed to be installed.
+fn compile_sass(manifest_dir: &Path, is_debug: bool) {
+    // In and out paths
+    let scss_dir = manifest_dir.join(SCSS_DIR);
+    let out_dir = manifest_dir.join(CSS_OUT_DIR);
+
     let minify_flags = ["-t", "compressed"];
     let flags = if is_debug { &[] as &[_] } else { &minify_flags };
 
--- src/template/mod.rs
+++ src/template/mod.rs
@@ -156,6 +156,7 @@ impl Page {
                 link rel="stylesheet" href="/static/main.css";
                 link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Courgette"
                 meta name="viewport" content="width=device-width, initial-scale=1.0";
+                script type="text/javascript" src="/static/main.js" {}
                 title {
                     (self.title)
                     ({
